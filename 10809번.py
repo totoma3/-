8cac8aa33fe96f58dfc62fe4b0e15cdc7cb2ee95
totoma3 <totoma3@naver.com>
2021-06-11 21:10:07 +0900

    Create 10809번.py

---
+++ 10809번.py
@@ -0,0 +1,5 @@
+#알파벳 소문자로만 이루어진 단어 S가 주어진다. 각각의 알파벳에 대해서, 단어에 포함되어 있는 경우에는 처음 등장하는 위치를, 포함되어 있지 않은 경우에는 -1을 출력하는 프로그램을 작성하시오.
+word=input()
+alphabet=list(range(97,123))
+for x in alphabet:
+    print(word.find(chr(x)))
